83cbe4f655030e0f913081062b2c23563c2f2f8e
Adam Fahey <afahey87@gmail.com>
2017-09-08 14:58:43 -0400

    lists.py
    lists.py

---
+++ lists.py
@@ -0,0 +1,20 @@
+
+cars = [ 'bmw', 'audi', 'vw', 'ford']
+print ("Here is the orginal list:")
+print (cars)
+
+print ("\nHere is the sorted list:")
+print (sorted(cars))
+
+print ("\nHere is the orginial list again:")
+print (cars)
+
+print ("\nHere is the cars in revers order: ")
+cars.reverse()
+print (cars)
+
+print ("\nHere is the legth of the cars:")
+print (len(cars))
+
+#This is an idex error. Indexing starts at 0 not 1. There is no item 4. 
+#print (cars[4])
